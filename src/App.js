92a03933f862f463fd14b2dc893b78727d2fc6e4
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-08-22 20:58:57 +0100

    Using a fake REST API to fetch Data

--- src/App.js
+++ src/App.js
@@ -21,18 +21,30 @@ class App extends Component {
         this.setState({searchfield:event.target.value})
     }
 
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users").then((response)=>{
+            return response.json();
+        }).then((users)=>{
+            this.setState({robots:users})
+        })
+    }
+
 
     render() {
         const filteredRobots = this.state.robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        return (
-            <div className="tc">
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length===0){
+            return <h1>Loading</h1>
+        }else {
+            return (
+                <div className="tc">
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }        
     }    
 };
 
