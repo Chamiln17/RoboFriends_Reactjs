0c4b61a7782f3d93f312cbacd00d20afd3502a0b
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-08-22 17:39:13 +0100

    Created CardList Component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from "./robots";
@@ -10,9 +10,7 @@ import { robots } from "./robots";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} email={robots[0].email} name={robots[0].name} />
-    <Card id={robots[1].id} email={robots[1].email} name={robots[1].name} />
-    <Card id={robots[2].id} email={robots[2].email} name={robots[2].name} />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
