facd5849851ded47b08c662df61c22a8b55b2cf6
Chamel Nadir Bouacha <lc_bouacha@esi.dz>
2023-08-22 19:28:05 +0100

    Created search functionality and added styles

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchBox =({searchChange ,searchField})=>{
+    return ( 
+        <div className="pa2 tc">
+            <input className="pa3 ba b--green bg-lightest-blue " type="search" 
+            placeholder="Search A Robot"
+            onChange={searchChange}
+             />
+
+        </div>
+    );
+};
+export default SearchBox;
